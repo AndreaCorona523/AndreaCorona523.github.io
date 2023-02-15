6488ea930b1908bdc5652961143e7e4701dd9c36
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2023-02-15 15:47:43 -0600

    Work Experience

---
+++ src/app/work-experience/work-experience.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience',
+  templateUrl: './work-experience.component.html',
+  styleUrls: ['./work-experience.component.css']
+})
+export class WorkExperienceComponent {
+  workExperience : Array<any> = [];
+  constructor() {}
+  ngOnInit(): void {
+    let work1 = {
+      fecha : "2021-2022",
+      ubicacion: "Toluca, Edo. Mex.",
+      puesto: "Vicepresidenta",
+      empresa: "Fetec",
+      logros: [
+        { descripcion: "Recoleccion de fondos"},
+        { descripcion: "Contacto con personas"},
+      ]
+    }
+
+    this.workExperience.push(work1);  
+  }
+
+}
